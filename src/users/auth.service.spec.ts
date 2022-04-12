b7ab694dc9389a87e51f14b9f749d6671d672f05
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-04-12 21:30:22 +0430

    Adding BeforeEach

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,14 +1,23 @@
-import { Test } from '@nestjs/testing';
+import { Test } from "@nestjs/testing";
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity'
 
-it('can create an instance of auth service', async () => {
-  // Create a fake copy of the users service
-  const fakeUsersService = {
+describe('AuthService', () => {
+  let service: AuthService;
+
+beforeEach(async () => {
+   // Create a fake copy of the users service
+  const fakeUsersService:Partial <UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password } as User),
   };
+
+
+  
+
+
 
   const module = await Test.createTestingModule({
     providers: [
@@ -21,6 +30,12 @@ it('can create an instance of auth service', async () => {
   }).compile();
 
   const service = module.get(AuthService);
+})
+it('can create an instance of auth service', async () => {
+ 
 
   expect(service).toBeDefined();
 });
+
+});
+
